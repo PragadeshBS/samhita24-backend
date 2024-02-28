8b89e74d524c6a313d795a18531c2016844ec64f
Pragadesh BS <pragadeshbs@protonmail.com>
2024-02-28 09:17:32 +0530

    Fix Access-Control-Allow-Methods header in server.js

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   res.header(
     "Access-Control-Allow-Headers",
