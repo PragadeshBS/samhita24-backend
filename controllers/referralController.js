4443710e6478d4eaf716393217cdef2b20d60d0e
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-03 13:22:52 +0530

    Add getAllReferrals endpoint to referralController and referralRoutes

--- controllers/referralController.js
+++ controllers/referralController.js
@@ -63,7 +63,22 @@ const getReferral = async (req, res) => {
   }
 };
 
+const getAllReferrals = async (req, res) => {
+  try {
+    const referrals = await Refferal.find({});
+    return res.status(200).json({
+      referrals,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   addReferral,
   getReferral,
+  getAllReferrals,
 };
--- routes/referralRoutes.js
+++ routes/referralRoutes.js
@@ -1,6 +1,7 @@
 const {
   getReferral,
   addReferral,
+  getAllReferrals,
 } = require("../controllers/referralController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
@@ -9,5 +10,7 @@ const router = require("express").Router();
 router.post("/", protect, adminOnly, addReferral);
 
 router.get("/:referralCode", protect, getReferral);
+
+router.get("/", protect, adminOnly, getAllReferrals);
 
 module.exports = router;
