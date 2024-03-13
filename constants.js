c95116eda874ad1627c002fcd3965350473d6e93
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-13 19:37:12 +0530

    Add new accommodation checkout option

--- constants.js
+++ constants.js
@@ -10,6 +10,7 @@ const accommodationCheckoutIdHoursMap = {
   accommodation_24hrs_no_food: 24,
   accommodation_48hrs: 48,
   accommodation_48hrs_no_food: 48,
+  accommodation_48hrs_no_food_new: 48,
 };
 
 module.exports = {
